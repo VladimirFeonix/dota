7c5f0828c24184e98d1124f2815e634f21fe4bf7
VladimirFeonix <volodya_volodya_volkov_97volkov@Mail.ru>
2025-11-08 01:09:55 +0300

    Add files via upload

--- js/constants.js
+++ js/constants.js
@@ -22,7 +22,7 @@ export const TAP_DELAY = 300;
 // Attribute icon URLs from Dota 2 CDN
 export const ATTRIBUTE_ICONS = {
     strength: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_strength.png',
-    agility: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_agility.png',
-    intellect: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_intelligence.png',
+    agility: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_intelligence.png',
+    intellect: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_agility.png',
     universal: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_universal.png'
 };
